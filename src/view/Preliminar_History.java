45232fbe5663fdb9861fff8d13d9cf062c6b6ab2
DilanJr <neiferjr15@gmail.com>
2022-07-04 13:33:47 -0400

    Se está desarrollando el algoritmo para buscar el historico de las garantías

--- src/view/Preliminar_History.java
+++ src/view/Preliminar_History.java
@@ -11,9 +11,9 @@ public class Preliminar_History extends javax.swing.JPanel {
 
     public Preliminar_History() {
         initComponents();
-        
-         getInformation();
-        
+
+        getInformation();
+
     }
 
     @SuppressWarnings("unchecked")
@@ -27,14 +27,14 @@ public class Preliminar_History extends javax.swing.JPanel {
         jLabel_Shop = new javax.swing.JLabel();
         jLabel_DatePurchase = new javax.swing.JLabel();
         jLabel_DateRegister = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
+        jLabel_Registered_by = new javax.swing.JLabel();
         jLabel_Equipo = new javax.swing.JLabel();
         jLabel_Color = new javax.swing.JLabel();
         jLabel_DateSentTechnical = new javax.swing.JLabel();
         jLabel_Delivery_Technical = new javax.swing.JLabel();
-        jLabel14 = new javax.swing.JLabel();
-        jLabel15 = new javax.swing.JLabel();
-        jLabel16 = new javax.swing.JLabel();
+        jLabel_Entregado = new javax.swing.JLabel();
+        jLabel_DeliveryShop = new javax.swing.JLabel();
+        jLabel_SentShop = new javax.swing.JLabel();
         jLabel_Status_Technical = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jLabel19 = new javax.swing.JLabel();
@@ -65,8 +65,8 @@ public class Preliminar_History extends javax.swing.JPanel {
         jLabel_Serial.setFont(new java.awt.Font("Roboto", 0, 24)); // NOI18N
         jLabel_Serial.setForeground(new java.awt.Color(240, 240, 240));
         jLabel_Serial.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        jLabel_Serial.setText("Equipo:");
-        add(jLabel_Serial, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 40, 440, 40));
+        jLabel_Serial.setText(" Serial: 01245789958655");
+        add(jLabel_Serial, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 40, 440, 40));
 
         jLabel_Falla.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
         jLabel_Falla.setForeground(new java.awt.Color(240, 240, 240));
@@ -88,10 +88,10 @@ public class Preliminar_History extends javax.swing.JPanel {
         jLabel_DateRegister.setText("Fecha De Registro: ");
         add(jLabel_DateRegister, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 220, 380, -1));
 
-        jLabel7.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(240, 240, 240));
-        jLabel7.setText("Registrado Por:");
-        add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 260, 380, -1));
+        jLabel_Registered_by.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        jLabel_Registered_by.setForeground(new java.awt.Color(240, 240, 240));
+        jLabel_Registered_by.setText("Registrado Por:");
+        add(jLabel_Registered_by, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 260, 380, -1));
 
         jLabel_Equipo.setFont(new java.awt.Font("Roboto", 0, 24)); // NOI18N
         jLabel_Equipo.setForeground(new java.awt.Color(240, 240, 240));
@@ -102,8 +102,8 @@ public class Preliminar_History extends javax.swing.JPanel {
         jLabel_Color.setFont(new java.awt.Font("Roboto", 0, 24)); // NOI18N
         jLabel_Color.setForeground(new java.awt.Color(240, 240, 240));
         jLabel_Color.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        jLabel_Color.setText("Equipo:");
-        add(jLabel_Color, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 40, 440, 40));
+        jLabel_Color.setText("Color: NEGRO ");
+        add(jLabel_Color, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 40, 430, 40));
 
         jLabel_DateSentTechnical.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
         jLabel_DateSentTechnical.setForeground(new java.awt.Color(240, 240, 240));
@@ -115,35 +115,35 @@ public class Preliminar_History extends javax.swing.JPanel {
         jLabel_Delivery_Technical.setText("Llevado Por:");
         add(jLabel_Delivery_Technical, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 340, 380, -1));
 
-        jLabel14.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
-        jLabel14.setForeground(new java.awt.Color(240, 240, 240));
-        jLabel14.setText("Entregado O Archivado: ");
-        add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 420, 380, -1));
+        jLabel_Entregado.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        jLabel_Entregado.setForeground(new java.awt.Color(240, 240, 240));
+        jLabel_Entregado.setText("Entregado O Archivado: ");
+        add(jLabel_Entregado, new org.netbeans.lib.awtextra.AbsoluteConstraints(465, 420, 380, -1));
 
-        jLabel15.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
-        jLabel15.setForeground(new java.awt.Color(240, 240, 240));
-        jLabel15.setText("Llevado Por:");
-        add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 380, 380, -1));
+        jLabel_DeliveryShop.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        jLabel_DeliveryShop.setForeground(new java.awt.Color(240, 240, 240));
+        jLabel_DeliveryShop.setText("Llevado Por:");
+        add(jLabel_DeliveryShop, new org.netbeans.lib.awtextra.AbsoluteConstraints(465, 380, 380, -1));
 
-        jLabel16.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
-        jLabel16.setForeground(new java.awt.Color(240, 240, 240));
-        jLabel16.setText("Enviado A Tienda:");
-        add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 340, 380, -1));
+        jLabel_SentShop.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        jLabel_SentShop.setForeground(new java.awt.Color(240, 240, 240));
+        jLabel_SentShop.setText("Enviado A Tienda:");
+        add(jLabel_SentShop, new org.netbeans.lib.awtextra.AbsoluteConstraints(465, 340, 380, -1));
 
         jLabel_Status_Technical.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
         jLabel_Status_Technical.setForeground(new java.awt.Color(240, 240, 240));
         jLabel_Status_Technical.setText("Estatus Técnico:");
-        add(jLabel_Status_Technical, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 220, 380, -1));
+        add(jLabel_Status_Technical, new org.netbeans.lib.awtextra.AbsoluteConstraints(465, 220, 380, -1));
 
         jSeparator1.setBackground(new java.awt.Color(240, 240, 240));
         jSeparator1.setForeground(new java.awt.Color(240, 240, 240));
         jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
-        add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(445, 92, 3, 320));
+        add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 50, 3, 390));
 
         jLabel19.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
         jLabel19.setForeground(new java.awt.Color(240, 240, 240));
         jLabel19.setText("Técnico:");
-        add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 100, 380, -1));
+        add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(465, 100, 380, -1));
 
         jScrollPane1.setBorder(null);
         jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -159,7 +159,7 @@ public class Preliminar_History extends javax.swing.JPanel {
         jTextArea_Comment_Technical.setWrapStyleWord(true);
         jScrollPane1.setViewportView(jTextArea_Comment_Technical);
 
-        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 260, 380, 60));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(465, 260, 380, 60));
 
         jScrollPane2.setBorder(null);
         jScrollPane2.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -175,24 +175,24 @@ public class Preliminar_History extends javax.swing.JPanel {
         jTextArea_Received.setWrapStyleWord(true);
         jScrollPane2.setViewportView(jTextArea_Received);
 
-        add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 140, 380, 60));
+        add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(465, 140, 380, 60));
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel15;
-    private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel19;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel_Color;
     private javax.swing.JLabel jLabel_DatePurchase;
     private javax.swing.JLabel jLabel_DateReceivedTechnical;
     private javax.swing.JLabel jLabel_DateRegister;
     private javax.swing.JLabel jLabel_DateSentTechnical;
+    private javax.swing.JLabel jLabel_DeliveryShop;
     private javax.swing.JLabel jLabel_Delivery_Technical;
+    private javax.swing.JLabel jLabel_Entregado;
     private javax.swing.JLabel jLabel_Equipo;
     private javax.swing.JLabel jLabel_Falla;
+    private javax.swing.JLabel jLabel_Registered_by;
+    private javax.swing.JLabel jLabel_SentShop;
     private javax.swing.JLabel jLabel_Serial;
     private javax.swing.JLabel jLabel_Shop;
     private javax.swing.JLabel jLabel_Status_Technical;
@@ -204,40 +204,44 @@ public class Preliminar_History extends javax.swing.JPanel {
     private javax.swing.JTextArea jTextArea_Received;
     // End of variables declaration//GEN-END:variables
 
-    private void getInformation(){
-        
+    private void getInformation() {
+
         try {
-            
+
             Connection cn = BD_Connection.connection();
             PreparedStatement pst = cn.prepareStatement("select * from warranty where id_warranty = '" + History.ID + "'");
-            
+
             ResultSet rs = pst.executeQuery();
-            
+
             if (rs.next()) {
-                
-                jLabel_Falla.setText("Falla: " + rs.getString("falla"));
-                jLabel_Shop.setText("Tienda: " + rs.getString("shop"));
-                jLabel_DatePurchase.setText("Fecha De Compra: " + rs.getString("date_purchase"));
-                jLabel_DateRegister.setText("Fecha De Registro: " + rs.getString("date_register"));
-                jLabel_DateSentTechnical.setText("Envaido A Servicio Técnico: " + rs.getString("date_sent_technical"));
-                jLabel_Delivery_Technical.setText("Llevado Por: " + rs.getString("delivery_technical"));
-                jLabel_DateReceivedTechnical.setText("Recibido En Servicio Técnico: " + rs.getString("date_received_technical"));
-                jTextArea_Comment_Technical.setText("Recibido Con: " + rs.getString("received"));
-                jLabel_Status_Technical.setText("Estatus Técnico: " + rs.getString("status_technical"));
-                jTextArea_Comment_Technical.setText("Comentarios Técnico: " + rs.getString("comments_technical"));
-                
+
+                jLabel_Serial.setText(" Serial: " + rs.getString("serial"));
+                jLabel_Falla.setText("Falla: " + '"' + rs.getString("falla") + '"');
+                jLabel_Shop.setText("Tienda: " + '"' + rs.getString("shop") + '"');
+                jLabel_DatePurchase.setText("Fecha De Compra: " + '"' + rs.getString("date_purchase") + '"');
+                jLabel_DateRegister.setText("Fecha De Registro: " + '"' + rs.getString("date_register") + '"');
+                jLabel_DateSentTechnical.setText("Enviado A Servicio Técnico: " + '"' + rs.getString("date_sent_technical") + '"');
+                jLabel_Delivery_Technical.setText("Llevado Por: " + '"' + rs.getString("delivery_technical") + '"');
+                jLabel_DateReceivedTechnical.setText("Recibido En Servicio Técnico: " + '"' + rs.getString("date_received_technical") + '"');
+                jTextArea_Comment_Technical.setText("Recibido Con: " + '"' + rs.getString("received") + '"');
+                jLabel_Status_Technical.setText("Estatus Técnico: " + '"' + rs.getString("status_technical") + '"');
+                jTextArea_Comment_Technical.setText("Comentarios Técnico: " + '"' + rs.getString("comments_technical") + '"');
+                jLabel_SentShop.setText("Enviado A Tienda: " + '"' + rs.getString("date_sent_shop") + '"');
+                jLabel_DeliveryShop.setText("Llevado Por: " + '"' + rs.getString("delivery_shop") + '"');
+                jLabel_Entregado.setText("Entregado O Archivado: " + '"' + rs.getString("status") + '"');
+
             }
-            
+
             cn.close();
-            
+
         } catch (SQLException e) {
-            
-            System.err.println("¡Error al consultar la información! " + e);
+
+            System.err.println("¡Error al consultar la información de la garantía! " + e);
             JOptionPane.showMessageDialog(null, "¡Error al consultar la información!", "¡Error!",
                     JOptionPane.OK_OPTION);
-            
+
         }
-        
+
     }
-    
+
 }
